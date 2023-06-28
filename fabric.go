4127af62148832418be19b5afb61183e88bb271e
misteeka <83140665+misteeka@users.noreply.github.com>
2023-06-28 14:05:58 +0300

    Fabric fix

--- fabric.go
+++ fabric.go
@@ -1,30 +1,31 @@
 package nonimus
 
 type Fabric[T any] struct {
-	Cache       chan *T
+	Cache       chan T
 	Concurrency int
 	CacheSize   int
 }
 
-func (f *Fabric[T]) Take() *T {
+func (f *Fabric[T]) Take() T {
 	return <-f.Cache
 }
 
-func NewFabric[T any](concurrency int, cacheSize int, machine func() *T) {
-	cache := make(chan *T, cacheSize)
+func NewFabric[T any](concurrency int, cacheSize int, scheme func() T) *Fabric[T] {
+	cache := make(chan T, cacheSize)
 	generationPool := NewPoolCollectorSize(concurrency, concurrency*16)
 	generationPool.AddTask(func() {
-		addTaskFunc[T](generationPool, cache, machine)
+		addTaskFunc[T](generationPool, cache, scheme)
 	})
+	return &Fabric[T]{Cache: cache, Concurrency: concurrency, CacheSize: cacheSize}
 }
 
-func addTaskFunc[T any](pool *Pool, cache chan *T, machine func() *T) {
+func addTaskFunc[T any](pool *Pool, cache chan T, scheme func() T) {
 	for {
 		if pool.concurrency == 1 {
-			cache <- machine()
+			cache <- scheme()
 		} else {
 			pool.AddTask(func() {
-				cache <- machine()
+				cache <- scheme()
 			})
 		}
 	}
